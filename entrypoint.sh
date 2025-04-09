d24fc9bf9887ce942a538d0e02453053751e8767
vbueno <viniciusbueno15@gmail.com>
2025-04-09 18:27:59 -0300

    (feat): Add new AI models to entrypoint script
    - Added Codellama, Codegemma, Gemma3, and Llama3.2 models
    - Improved logging with echo statements for each model download

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,17 @@ sleep 5
 
 echo "Baixando o modelo DeepSeek..."
 ollama pull deepseek-coder:1.3b
+
+echo "Baixando o modelo Codellama..."
 ollama pull codellama:7b
+
+echo "Baixando o modelo Codegemma..."
 ollama pull codegemma:2b
+
+echo "Baixando o modelo Gemma3..."
 ollama pull gemma3:4b
+
+echo "Baixando o modelo Llama3.2..."
 ollama pull llama3.2
 
 ollama list
